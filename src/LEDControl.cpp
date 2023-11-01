9f21d9acb4a0f4eef0b10d374eb9e9fbe8e9b060
Andy <azogg65@bluewin.ch>
2023-11-01 17:35:53 +0100

    GPIO assigment changed

--- src/LEDControl.cpp
+++ src/LEDControl.cpp
@@ -1,6 +1,25 @@
 #include "LEDControl.h"
 
-static uint8_t ChannelToGPIOMapping[16] = { GPIO_NUM_16, GPIO_NUM_17, GPIO_NUM_18, GPIO_NUM_19, GPIO_NUM_21, GPIO_NUM_22, GPIO_NUM_23, GPIO_NUM_26, GPIO_NUM_27, GPIO_NUM_25, GPIO_NUM_33, GPIO_NUM_32, GPIO_NUM_35, GPIO_NUM_34, GPIO_NUM_39, GPIO_NUM_36 };
+static uint8_t ChannelToGPIOMapping[16] = { 
+	GPIO_NUM_16, 
+	GPIO_NUM_17, 
+	GPIO_NUM_18, 
+	GPIO_NUM_19, 
+	GPIO_NUM_21, 
+	GPIO_NUM_22, 
+	GPIO_NUM_23, 
+	GPIO_NUM_26,
+
+	GPIO_NUM_12,
+	GPIO_NUM_13, 
+	GPIO_NUM_14, 
+	GPIO_NUM_4, 
+	GPIO_NUM_25, 
+	GPIO_NUM_27, 
+	GPIO_NUM_32, 
+	GPIO_NUM_33
+
+};
 
 // ===========================================
 // LED
@@ -8,10 +27,14 @@ static uint8_t ChannelToGPIOMapping[16] = { GPIO_NUM_16, GPIO_NUM_17, GPIO_NUM_1
 LED::LED(const uint8_t Channel_) :
 	Channel(Channel_) {
 
+	Serial.println("LED::LED");
+
 	PWMResolution = 8;
 	PWMFrequency = 5000;
 
 	GPIO = ChannelToGPIOMapping[Channel];
+	Serial.print("    Channel: "); Serial.println(Channel);
+	Serial.print("    GPIO   : "); Serial.println(GPIO);
 
 	ledcSetup(Channel, PWMFrequency, PWMResolution);
 	ledcAttachPin(GPIO, Channel);
