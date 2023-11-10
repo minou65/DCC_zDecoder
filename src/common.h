1a67372437cf00794075f01034c38c1662933c57
Andy <azogg65@bluewin.ch>
2023-11-10 15:50:49 +0100

    status changed

--- src/common.h
+++ src/common.h
@@ -56,11 +56,11 @@ static char DecoderModeNames[][STRING_LEN] = {
     "Fernseher (1)",  // ok
     "Schweisslicht (3)", // ok
     "Feuer (3)", // ok
-    "Blitzlicht (1)",
+    "Blitzlicht (1)", // ok
     "Lauflicht 1 (1..16)", // ok
     "Lauflicht 2 (1..16)",  // ok
     "Lauflicht 3 (1..16)",  // ok
-    "Lauflicht 4 (1..16)", 
+    "Lauflicht 4 (1..16)",  // ok
 
     "Hausbeleuchtung (1..16)", // ok
     "Neonlampen (1..16)", // ok
