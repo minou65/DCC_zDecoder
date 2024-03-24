95c350445c3f40c94cdc1c83444303f40a45f32b
Andy <azogg65@bluewin.ch>
2024-03-24 10:39:18 +0100

    thing name changed

--- src/webhandling.cpp
+++ src/webhandling.cpp
@@ -21,7 +21,7 @@
 #include "common.h"
 
 // -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
-const char thingName[] = "testThing";
+const char thingName[] = "zDecoder";
 
 // -- Initial password to connect to the Thing, when it creates an own Access Point.
 const char wifiInitialApPassword[] = "123456789";
