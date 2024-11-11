322b70885f0867adc0ee631aa6ef59c88b6f2f0b
Andy <azogg65@bluewin.ch>
2024-11-11 16:11:23 +0100

    Add SetMaxBrightness method to Feuer and Blitzlicht classes

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
-#define VERSION_PATCH 5
+#define VERSION_PATCH 6
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 685
+#define VERSION_BUILD 686
 #define VERSION_DATE "2024-11-11"
-#define VERSION_TIME "15:54:57"
-#define VERSION "1.2.5.685"
-#define VERSION_STR "1.2.5.685 (2024-11-11 15:54:57)"
+#define VERSION_TIME "16:09:06"
+#define VERSION "1.2.6.686"
+#define VERSION_STR "1.2.6.686 (2024-11-11 16:09:06)"
