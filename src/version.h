bc19ce47799452cf81811c9c975158c38ab84963
Andy <azogg65@bluewin.ch>
2025-08-07 07:26:58 +0200

    Refactor: streamline zDecoder.ino by removing unused code switching to ESP32_DecoderCore lib

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
-#define VERSION_PATCH 11
+#define VERSION_MINOR 3
+#define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 740
-#define VERSION_DATE "2025-05-30"
-#define VERSION_TIME "07:40:11"
-#define VERSION "1.2.11.740"
-#define VERSION_STR "1.2.11.740 (2025-05-30 07:40:11)"
+#define VERSION_BUILD 760
+#define VERSION_DATE "2025-08-07"
+#define VERSION_TIME "06:52:15"
+#define VERSION "1.3.0.760"
+#define VERSION_STR "1.3.0.760 (2025-08-07 06:52:15)"
--- src/zDecoder.ino
+++ src/zDecoder.ino
@@ -4,339 +4,97 @@
  Author:	andy
 */
 
-// #define NOTIFY_DCC_ACCSTATE_MSG
-// #define NOTIFY_DCC_MSG
-// #define NOTIFY_TURNOUT_MSG
-//#define NOTIFY_DCC_CV_WRITE_MSG
-//#define NOTIFY_DCC_CV_CHANGE_MSG
-//#define DEBUG_MSG
+#include <Arduino.h>
 
+#include "version.h"
+#include "pinmapping.h"
 
 #include "webhandling.h"
-#include "Vector.h"  // https://github.com/tomstewart89/Vector/fork
-#include <NmraDcc.h>
-#include "accessories.h"
-#include "Blinkers.h"
-#include "SBB_Signals.h"
-#include "Turnouts.h"
-#include "Watchdog.h"
-#include "NMRAhandling.h"
-#include "pinmapping.h"
-#include "version.h"
+
+#include "ESP32_DecoderCore.h"
 
 char Version[] = VERSION_STR;
-
-// #include "NMRAhandling.h"
-#include "Vector.h"
-
-// Liste mit allen Decoderobjekten (Apps)
-Vector<accessories*> decoder;
 
 // Task handle (Core 0 on ESP32)
 TaskHandle_t TaskHandle;
 
 bool ResetDCCDecoder = false;
 
-void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower) {
-#ifdef  NOTIFY_TURNOUT_MSG
-	Serial.print("notifyDccAccTurnoutOutput: Turnout: ");
-	Serial.print(Addr, DEC);
-	Serial.print(" Direction: ");
-	Serial.print(Direction ? "Closed" : "Thrown");
-	Serial.print(" Output: ");
-	Serial.println(OutputPower ? "On" : "Off");
-#endif
-
-	for (int _i = 0; _i < decoder.Size(); _i++) {
-		decoder[_i]->notifyTurnoutAddress(Addr, Direction, OutputPower);
-	}
-}
-
-void notifyDccSpeed(uint16_t Addr, uint8_t Speed, uint8_t ForwardDir, uint8_t SpeedSteps) {
-	for (int i = 0; i < decoder.Size(); i++) {
-
-		// is it a Servo?
-		if (decoder[i]->ChannelMode() == 230) {
-			decoder[i]->notifyDccSpeed(Addr, Speed, ForwardDir, SpeedSteps);
-		}
-	}
-}
-
-void notifyDccAccState(uint16_t Addr, uint16_t BoardAddr, uint8_t OutputAddr, uint8_t State) {
-	uint8_t _OutputNum = OutputAddr >> 1;  //shift over the bits so the outputaddr is 0 to 3
-	uint8_t _Cmd = OutputAddr & 0b00000001;  //JMRI puts out the state as the right most bit of pDccMsg->Data[1], the state argument doesnt change in JMRI Turnout.
-
-#ifdef NOTIFY_DCC_ACCSTATE_MSG
-	Serial.print("AccState - ");
-	Serial.print("Raw addr: ");
-	Serial.print(Addr);
-	Serial.print(" BoardAddr: ");
-	Serial.print(BoardAddr, DEC);
-	Serial.print(" OutputAddr: ");
-	Serial.print(OutputAddr, DEC);
-	Serial.print(" Output: ");
-	Serial.print(_OutputNum);
-	Serial.print(" State: 0b");
-	Serial.print(State, BIN);
-	Serial.print(" Command: ");
-	Serial.print(_Cmd == 1 ? "Closed" : "Thrown");
-	Serial.println();
-	//Serial.print("    zDecoder: "); Serial.println(BoardAddr + OutputAddr);
-#endif 
-
-#ifdef  NOTIFY_TURNOUT_MSG
-	Serial.print("zDecoder: Address"); Serial.print(Addr);
-	Serial.print(_Cmd == 1 ? " :Closed" : " :Thrown");
-	Serial.println();
-#endif
-
-	for (int _i = 0; _i < decoder.Size(); _i++) {
-		decoder[_i]->notifyAddress(Addr, _Cmd);
-	}
-}
-
-void notifyDccFunc(uint16_t Addr, uint8_t FuncNum, uint8_t FuncState){
-
-	if (FuncNum != 1)
-		return;
-
-	for (int i = 0; i < decoder.Size(); i++) {
-
-		// is it a Servo?
-		if (decoder[i]->ChannelMode() == 230) {
-			if (FuncState & 0x10)
-				decoder[i]->on();
-			else
-				decoder[i]->off();
-		}
-	}
-
-	if (FuncNum != 1)
-		return;
-}
-
-void handleDecoderGroup(uint8_t DecoderGroup) {
-	if (DecoderGroupIsActive(DecoderGroup)) {
-		if (decoder[DecoderGroup]->isOn()) {
-			decoder[DecoderGroup]->off();
-		}
-		else {
-			decoder[DecoderGroup]->on();
-		}
-	}
-}
-
-bool DecoderGroupIsEnabled(uint8_t DecoderGroup) {
-	if (DecoderGroupIsActive(DecoderGroup)) {
-		return decoder[DecoderGroup]->isOn();
-	}
-	else {
-		return false;
-	}
-}
-
-bool DecoderGroupIsActive(uint8_t DecoderGroup) {
-	if (DecoderGroup <= decoder.Size() && decoder[DecoderGroup] != nullptr) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
 
 void zDecoderReset() {
 	OutputGroup* outputgroup_ = &OutputGroup1;
 	while (outputgroup_ != nullptr) {
-		outputgroup_->DesignationParam.applyDefaultValue();
-		outputgroup_->ModeParam.applyDefaultValue();
-		outputgroup_->NumberParam.applyDefaultValue();
-		outputgroup_->AddressParam.applyDefaultValue();
-		outputgroup_->TimeOnParam.applyDefaultValue();
-		outputgroup_->TimeOffParam.applyDefaultValue();
-		outputgroup_->TimeOnFadeParam.applyDefaultValue();
-		outputgroup_->TimeOffFadeParam.applyDefaultValue();
+		outputgroup_->applyDefaultValues();
 		outputgroup_->setActive(false);
 
 		outputgroup_ = (OutputGroup*)outputgroup_->getNext();
 	}
-
-	ServoGroup* servogroup_ = &ServoGroup1;
-	while (servogroup_ != nullptr) {
-		servogroup_->DesignationParam.applyDefaultValue();
-		servogroup_->AddressParam.applyDefaultValue();
-		servogroup_->TravelTimeParam.applyDefaultValue();
-		servogroup_->Limit1Param.applyDefaultValue();
-		servogroup_->Limit2Param.applyDefaultValue();
-		servogroup_->setActive(false);
-
-		servogroup_ = (ServoGroup*)servogroup_->getNext();
-	}
 }
 
 void zDecoderInit(void) {
-	uint8_t channel_ = 0;
+	decoder.reset(); // Clear the decoder list	
 
-	// Decoderobjekte in decoder löschen
-	decoder.Erase(0, decoder.Size());
-	decoder.Clear();
-	Vector<accessories*>().Swap(decoder);
-
+	Serial.println("======== Setting up outputs... =========");
+	uint8_t OutputIndex_ = 0;
+	uint8_t totalAssignedPins_ = 0;
 	OutputGroup* outputgroup_ = &OutputGroup1;
 	while (outputgroup_ != nullptr) {
-		if (channel_ > 15) {
-			Serial.println("no more free channels!");
+		if (ESP32PWM::PWMCount > NUM_PWM) {
+			Serial.print("No more pwm channels available for servos! max: "); Serial.println(NUM_PWM);
+			break;
+		}
+
+		if (OutputIndex_ >= MAX_OUTPUT_PINS) {
+			Serial.println("No more output pins available!");
 			break;
 		}
 
 		if (outputgroup_->isActive()) {
-			uint8_t Mode_ = atoi(outputgroup_->ModeValue);
-			uint8_t Count_ = atoi(outputgroup_->NumberValue);
-			uint16_t Address_ = atoi(outputgroup_->AddressValue);
-			uint8_t TimeOn_ = atoi(outputgroup_->TimeOnValue);
-			uint8_t TimeOff_ = atoi(outputgroup_->TimeOffValue);
-			uint8_t Multiplier_ = atoi(outputgroup_->MultiplierValue); // Multiplikator
-			uint8_t TimeOnFade_ = atoi(outputgroup_->TimeOnFadeValue);
-			uint8_t TimeOffFade_ = atoi(outputgroup_->TimeOffFadeValue);
-			uint8_t Brightness_ = atoi(outputgroup_->BrightnessValue);
+			// Read parameters
+			uint8_t Mode_ = outputgroup_->getMode();
+			uint8_t assignedPins_ = outputgroup_->getNumber();
+			uint16_t Address_ = outputgroup_->getAddress();
+			uint16_t TimeOn_ = outputgroup_->getTimeOn();
+			uint16_t TimeOff_ = outputgroup_->getTimeOff();
+			uint16_t Multiplier_ = outputgroup_->getMultiplier();
+			uint16_t TimeOnFade_ = outputgroup_->getTimeOnFade();
+			uint16_t TimeOffFade_ = outputgroup_->getTimeOffFade();
+			uint8_t Brightness_ = outputgroup_->getBrightness();
 
-			uint16_t DayLightAddress_ = 0;
-			uint8_t DayBrightness_ = 255;
-			uint8_t NightBrightness_ = 150;
+			Serial.printf("Values for output index %u loaded\n", OutputIndex_);
+			Serial.printf("    Pins assigned to this output group:: %u\n", assignedPins_);
+			Serial.printf("    Address: %u\n", Address_);
+			Serial.print(F("    Mode: ")); Serial.println(Mode_);
 
-			Serial.print(F("Values for channel ")); Serial.print(channel_); Serial.println(F(" preserved"));
-			Serial.print(F("    Channels used: ")); Serial.println(Count_);
-			Serial.print(F("    Address: ")); Serial.println(Address_);
+			// Create accessory
+			Accessory* newAccessory_ = createOutputAccessory(Mode_, Address_, OutputIndex_, assignedPins_, TimeOn_, TimeOff_, Multiplier_, TimeOnFade_, TimeOffFade_);
 
-			// Einrichten des Ports
-			accessories* newAccessory = nullptr;
-			switch (Mode_) {
-			case 0:
-				newAccessory = new accessories(Address_, channel_);
-				channel_ += Count_;
-				break;
-			case 1: // Watchdog
-				newAccessory = new Watchdog(Address_, channel_, Multiplier_ * TimeOn_);
-				channel_ += 1;
-				break;
-			case 40: // einfacher Ausgang
-				newAccessory = new Ausgang(Address_, channel_);
-				channel_ += 1;
-				break;
-			case 50: // Blinker
-				newAccessory = new Blinker(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_, TimeOnFade_, TimeOffFade_, Mode_);
-				channel_ += 1;
-				break;
-			case 51: // Wechselblinker
-				newAccessory = new Wechselblinker(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_, TimeOnFade_, TimeOffFade_);
-				channel_ += 2;
-				break;
-			case 52: case 53: case 54: case 55: // diverse Lauflichter
-				newAccessory = new Lauflicht(Address_, channel_, Count_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_, Mode_);
-				channel_ += Count_;
-				break;
-			case 60: // Hausbeleuchtung
-				newAccessory = new Hausbeleuchtung(Address_, channel_, Count_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_);
-				channel_ += Count_;
-				break;
-			case 61: // Neonröhren
-				newAccessory = new NeonLampen(Address_, channel_, Count_, Multiplier_);
-				channel_ += Count_;
-				break;
-			case 62: // Natriumlampen
-				newAccessory = new NatriumLampen(Address_, channel_, Count_, Multiplier_, TimeOnFade_, TimeOffFade_);
-				channel_ += Count_;
-				break;
-			case 80: // TV
-				newAccessory = new Fernseher(Address_, channel_);
-				channel_ += 1;
-				break;
-			case 81: // Schweisslicht
-				newAccessory = new Schweissen(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_);
-				channel_ += 3;
-				break;
-			case 82: // Feuer
-				newAccessory = new Feuer(Address_, channel_);
-				channel_ += 3;
-				break;
-			case 83: // Blitzlicht
-				newAccessory = new Blitzlicht(Address_, channel_, TimeOnFade_ * 1000, TimeOffFade_ * 1000, TimeOn_ * Multiplier_, TimeOff_ * Multiplier_);
-				channel_ += 1;
-				break;
-			case 102: // SBB_Hauptsignal_102
-				newAccessory = new SBB_Hauptsignal_102(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 2;
-				break;
-			case 103: // SBB_Hauptsignal_103
-				newAccessory = new SBB_Hauptsignal_103(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 3;
-				break;
-			case 104: // SBB_Hauptsignal_104
-				newAccessory = new SBB_Hauptsignal_104(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 4;
-				break;
-			case 105:
-				newAccessory = new SBB_Hauptsignal_105(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 5;
-				break;
-			case 106:
-				newAccessory = new SBB_Hauptsignal_106(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 6;
-				break;
-			case 110:
-				newAccessory = new SBB_Zwergsignal_110(Address_, channel_, DayLightAddress_, DayBrightness_, NightBrightness_, TimeOnFade_, TimeOffFade_);
-				channel_ += 2;
-				break;
-			case 201: // Spule, Entkuppler
-				newAccessory = new UnCoupler(channel_, Address_, Multiplier_ * TimeOn_);
-				channel_ += 1;
-				break;
-			case 202: // Weiche
-				newAccessory = new Turnout(channel_, channel_ + 1, Address_, Multiplier_ * TimeOn_);
-				channel_ += 2;
-				break;
-			}
+			if (newAccessory_ != nullptr) {
+				OutputIndex_ += newAccessory_->getUsedPins(); // Increase OutputIndex_ by the number of used pins
 
-			if (newAccessory != nullptr) {
-				Serial.print(F("    Mode: ")); Serial.println(Mode_);
-
-				// Check the type of newAccessory
-				if (newAccessory->getType() == AccessoryType::LED) {
-					Ausgang* ledAccessory = static_cast<Ausgang*>(newAccessory);
-					// If the cast is successful, set the brightness
+				// Set brightness if it's a LED accessory (before wrapping in SoundAccessory)
+				if (newAccessory_->getType() == AccessoryType::LED) {
+					Ausgang* ledAccessory = static_cast<Ausgang*>(newAccessory_);
 					ledAccessory->SetMaxBrightness(Brightness_);
 				}
-				decoder.PushBack(newAccessory);
+
+				decoder.add(newAccessory_);
 			}
+			totalAssignedPins_ += assignedPins_;
+
+			Serial.println(F("*****************************"));
 		}
+
 		outputgroup_ = (OutputGroup*)outputgroup_->getNext();
 	}
 
-	ServoGroup* servogroup_ = &ServoGroup1;
-	while (servogroup_ != nullptr) {
-		if (channel_ > 15) {
-			Serial.println("no more free channels!");
-			break;
-		}
+	Serial.print("Decoder size: "); Serial.println(decoder.getSize());
+	Serial.print("Total assigned outputs: "); Serial.println(totalAssignedPins_);
+	Serial.print("Total assigned PWM channels: "); Serial.println(ESP32PWM::PWMCount);
 
-		if (servogroup_->isActive()) {
-			uint8_t Address_ = atoi(servogroup_->AddressValue);
-			uint8_t _TravelTime = atoi(servogroup_->TravelTimeValue);
-			uint8_t Multiplier_ = atoi(servogroup_->MultiplierValue); // Multiplikator
-			uint8_t _Limit1 = atoi(servogroup_->Limit1Value);
-			uint8_t _Limit2 = atoi(servogroup_->Limit2Value);
-			uint8_t Count_ = 1;
 
-			Serial.print(F("Values for channel ")); Serial.print(channel_); Serial.println(F(" preserved"));
-			Serial.print(F("    Channels used: ")); Serial.println(Count_);
+	Serial.println(F("======== zdecoder initialization complete ========="));
 
-			accessories* newServo = new TurnoutServo(channel_, Address_, _Limit1, _Limit2, Multiplier_ * _TravelTime);
-			decoder.PushBack(newServo);
-			channel_ += 1;
-
-			Serial.print(F("next channel is ")); Serial.println(channel_);
-		}
-		servogroup_ = (ServoGroup*)servogroup_->getNext();
-	}
 }
 
 void setup() {
@@ -347,16 +105,14 @@ void setup() {
 
 	Serial.println("Starting with Firmware " + String(VERSION));
 
-	for (uint8_t _i = 0; _i < sizeof(ChannelToGPIOMapping) - 1; _i++) {
-		pinMode(ChannelToGPIOMapping[_i], OUTPUT);
-		digitalWrite(ChannelToGPIOMapping[_i], LOW);
-	}
+	// Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
 
 	NMRAsetup();
-	Serial.println("NMRA initalized");
-
 	websetup();
-	Serial.println("web server initalized");
 
 	if (CONFIG_PIN >= 0) {
 		pinMode(CONFIG_PIN, INPUT_PULLUP);
@@ -395,8 +151,7 @@ void loop2(void* parameter) {
 			ResetDCCDecoder = false;
 		}
 
-		for (int _i = 0; _i < decoder.Size(); _i++) {
-			decoder[_i]->process();
-		}
+		decoder.process(); // Process all decoder accessories
+		vTaskDelay(10 / portTICK_PERIOD_MS); // Delay to prevent watchdog timeout
 	}
 }
